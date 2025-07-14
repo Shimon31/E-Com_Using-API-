d2470f7d05f995dbc174225fd7dcd3400d54cc05
Shimon <shimonbhuiya02@gmail.com>
2025-07-14 19:39:22 +0600

    auth repo started

---
+++ app/src/main/java/com/example/e_commerce_practice/data/repo/AuthRepo.kt
@@ -0,0 +1,12 @@
+package com.example.e_commerce_practice.data.repo
+
+import com.example.e_commerce_practice.data.model.login.RequestLogin
+
+class AuthRepo {
+
+
+    fun login(requestLogin: RequestLogin){
+
+    }
+
+}
